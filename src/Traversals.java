a75d9f7cbc9d459118a98d7dd222c50badda6e25
Connor <connorjohnhughes@gmail.com>
2026-03-10 13:54:50 -0700

    Whole new node livecode 2

--- src/Traversals.java
+++ src/Traversals.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Traversals {
   public static void main(String[] args) {
@@ -41,11 +44,57 @@ public class Traversals {
     child3_1.children.add(child3_1_2);
 
 
+    Map<Integer, List<Integer>> tree = new HashMap<>();
+    tree.put(33,List.of(24,61,12));
+    tree.put(17,List.of(5,99));
+    tree.put(58,List.of(73));
+    tree.put(24,List.of(83,6));
+    tree.put(24,List.of(83,6));
+    tree.put(88,List.of(17,58,33));
+    // leafs
+    tree.put(5, new ArrayList<>());
+    tree.put(99, new ArrayList<>());
+    tree.put(73, new ArrayList<>());
+    tree.put(83, new ArrayList<>());
+    tree.put(6, new ArrayList<>());
+    tree.put(61, new ArrayList<>());
+    tree.put(12, new ArrayList<>());
+
+  
+
 
     // preOrder(root);
-    System.out.println(max(root));
+    // System.out.println(max(root));
+
+    // System.out.println(tree);
+    preOrderMap(tree, 88);
+
+
   };
 
+
+  static void preOrderMap (Map<Integer, List<Integer>> tree, int current){
+
+    // what if current is not in tree
+    if(!tree.containsKey(current)) return;
+
+    // print current
+    System.out.println(current);
+
+    // recurse its children
+    List<Integer> children = tree.get(current);
+
+    for (Integer child : children) {
+      preOrderMap(tree, child);
+    };
+
+
+
+
+
+
+
+  }
 
 
   static void preOrder(Node<?> node){
@@ -77,5 +126,7 @@ public class Traversals {
 
 
     return biggest;
-  }
+  }   
+
+
 }
